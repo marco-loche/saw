d70897771ea389e3eb5210e02d10bd5d5e299ee8
Marco Loche <marco.loche@capturator.com>
2014-11-06 21:18:31 +0100

    the SAW object is now able to find the API adapter

--- __tests__/saw-test.js
+++ __tests__/saw-test.js
@@ -27,8 +27,38 @@ describe('saw', function () {
     expect(this.saw.isInitialized()).toBe(false);
   });
 
-  it('should be initilized after init invocation', function () {
-    this.saw.initialize();
-    expect(this.saw.initialized).toBe(true);
+  describe(' with a not available API Adapter', function () {
+
+    it('should throw an error if no API Adapter is available', function () {
+      expect(this.saw.initialize).toThrow("A valid SCORM API Adapter can not be found in the window or in the window.opener");
+      expect(this.saw.isInitialized()).toBe(false);
+    });
+  });
+
+  describe(' with an available API Adapter', function () {
+
+    it('should be initilized after init invocation if the adapted is defined in the current window', function () {
+      window.API = {};
+      this.saw.initialize();
+      expect(this.saw.isInitialized()).toBe(true);
+    });
+
+    it('should be initilized after init invocation if the API is defined in the window.opener', function () {
+      var opener = window.opener;
+      var parent = {};
+      // building the nested parent structure with max level of recursion
+      for(var i = 0; i++; i <= 7){
+        parent = { 'parent': parent };
+      }
+      window.parent = parent;
+      window.opener = {}
+      window.opener.API = {};
+
+      this.saw.initialize();
+      expect(this.saw.isInitialized()).toBe(true);
+      window.opener = opener;
+    });
+
+
   });
 });
--- saw.js
+++ saw.js
@@ -1,17 +1,50 @@
 //     saw.js 0.0.0
 //     (c) 2014 Marco Loche
-//     Underscore may be freely distributed under the MIT license.
+//     SAW may be freely distributed under the MIT license.
+//     http://marco-loche.mit-license.org/
 "use strict";
 
 (function () {
 
+  var findAPITries = 0;
+  // The function charged to locate the API adapter object presented by the LMS.
+  // As described in section 3.3.6.1 of the documentation.
+  var findAPI = function (win) {
+    if (win == null) {
+      return null;
+    }
+    while ((win.API == null) && (win.parent != null) && (win.parent != win)) {
+      findAPITries++;
+      if (findAPITries > 7) {
+        return null;
+      }
+      win = win.parent;
+    }
+
+    return win.API;
+  };
+
   var saw = {
-    initialized:   false,
-    initialize:    function () {
+    initialized: false,
+    API:         null,
+
+    initialize: function () {
       this.initialized = true;
-    },
+      this.API = findAPI(window);
+      if ((this.API == null) && (window.opener != null) && (typeof(window.opener) != "undefined")) {
+        this.API = findAPI(window.opener);
+      }
+
+      if (this.API == null) {
+        throw new Error("A valid SCORM API Adapter can not be found in the window or in the window.opener");
+      }
+      this.initialized = true;
+
+    }
+    ,
+
     isInitialized: function () {
-      return this.initialized;
+      return (this.initialized && this.API != null);
     }
 
   }
